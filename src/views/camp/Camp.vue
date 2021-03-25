1c58fde910773bc62b26577c6c9e0669b3b67ed6
hippocampus <1577369187@qq.com>
2021-03-25 18:41:30 +0800

    营地列表

--- src/views/camp/Camp.vue
+++ src/views/camp/Camp.vue
@@ -120,7 +120,7 @@ export default {
       goAddCamp() {
           this.$router.push('/params')
       },
-      //删除营地
+      //删除营地         
       delectCamp(index) {
         this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
           confirmButtonText: '确定',
